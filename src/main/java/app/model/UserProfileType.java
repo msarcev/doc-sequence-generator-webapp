6b720a7de4ee5ee94192b1669b26a2e7a6ef217c
msarcev <mario.sarcevic.985@gmail.com>
2016-10-10 23:16:32 +0200

    hibernate jdbc config

---
+++ src/main/java/app/model/UserProfileType.java
@@ -0,0 +1,18 @@
+package app.model;
+
+public enum UserProfileType {
+
+    USER("USER"),
+    ADMIN("ADMIN");
+
+    String userProfileType;
+
+    private UserProfileType(String userProfileType) {
+        this.userProfileType = userProfileType;
+    }
+
+    public String getUserProfileType() {
+        return userProfileType;
+    }
+
+}
