7c45d8c42ba00ab94c9f11892e4ccdb661040fd4
msarcev <mario.sarcevic.985@gmail.com>
2016-10-11 21:39:02 +0200

    cleaning

--- src/main/java/app/conf/WebSecurityConfig.java
+++ src/main/java/app/conf/WebSecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
@@ -31,6 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 				.and()
 				.logout()
 				.permitAll();
+
 	}
 
 	@Autowired
@@ -39,5 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		auth
 				.inMemoryAuthentication()
 				.withUser("admin").password("admin").roles("USER");
+
 	}
+
 }
